7a37e2156b04d9a0e652bbc79d479b61b0cbe98c
Rosa Richter <cosmic.lady.rosa@gmail.com>
2022-12-27 19:06:21 -0700

    start to match queries!

--- lib/cesql.ex
+++ lib/cesql.ex
@@ -3,4 +3,24 @@ defmodule CESQL do
   Documentation for `CESQL`.
   """
 
+  
+  def query(event, cesql) do
+    {:ok, ast} = parse_cesql(cesql)
+
+    case ast do
+      {:keyword, _, field} ->
+        field = List.to_string(field)
+        if field in ~w(specversion source id type data datacontenttype dataschema subject time) do
+          field = String.to_existing_atom(field)
+          Map.fetch!(event, field)
+        else
+          Map.get(event.extensions, field)
+        end
+    end
+  end
+
+  defp parse_cesql(str) do
+    {:ok, tokens, _} = str |> String.to_charlist() |> :cesql_lexer.string()
+    :cesql_parser.parse(tokens)
+  end
 end
--- test/cesql_test.exs
+++ test/cesql_test.exs
@@ -1,4 +1,29 @@
 defmodule CESQLTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest CESQL
+
+  test "extract field from CloudEvent" do
+    event =
+      Cloudevents.from_map!(%{
+        specversion: "1.0",
+        source: "myapp",
+        id: "abc123",
+        type: "some.event"
+      })
+
+    assert CESQL.query(event, "source") == "myapp"
+  end
+
+  test "extract extension field from CloudEvent" do
+    event =
+      Cloudevents.from_map!(%{
+        specversion: "1.0",
+        source: "myapp",
+        id: "abc123",
+        type: "some.event",
+        sequence: 100
+      })
+
+    assert CESQL.query(event, "sequence") == 100
+  end
 end
